c1fdf0abef901d673051952cd2d976f6a64c04e6
HelgaIngridHochbauer <162413766+HelgaIngridHochbauer@users.noreply.github.com>
2024-10-16 16:07:55 +0300

    Add files via upload

---
+++ Project PR/src/CampaignFunctions.java
@@ -0,0 +1,21 @@
+public class CampaignFunctions {
+
+    public static double analyzeFollowerGrowth(int initialFollowers, int currentFollowers) {
+        if (initialFollowers == 0) {
+            return 0.0; // Avoid division by zero
+        }
+        return ((double) (currentFollowers - initialFollowers) / initialFollowers) * 100;
+    }
+
+    public static int analyzeCampaignEngagement(Campaign campaign) {
+        int totalLikes = 0;
+        for (Post post : campaign.getPosts()) {
+            totalLikes += post.getExpectedLikes();
+        }
+        return totalLikes;
+    }
+
+    public static int analyzePostEngagement(Post post) {
+        return post.getExpectedLikes();
+    }
+}
---
+++ Project PR/src/Influencer.java
@@ -0,0 +1,34 @@
+public class Influencer {
+    private String name;
+    private String platform;
+    private int followers;
+
+    // Constructor
+    public Influencer(String name, String platform, int followers) {
+        this.name = name;
+        this.platform = platform;
+        this.followers = followers;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public int getFollowers() {
+        return followers;
+    }
+
+    @Override
+    public String toString() {
+        return "Influencer{" +
+                "name='" + name + '\'' +
+                ", platform='" + platform + '\'' +
+                ", followers=" + followers +
+                '}';
+    }
+}
